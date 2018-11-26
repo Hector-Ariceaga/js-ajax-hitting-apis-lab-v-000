1a2ad34ec2dc7cbb2910b040c4efa3b0b0413e69
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-26 21:43:02 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function getRepositories() {
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
   console.log(commits)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>
 }
 
 function getCommits(el) {
