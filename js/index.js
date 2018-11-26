9acf50cc8353a19a33dc5fbeb56e9f3c36e632b9
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-26 20:52:08 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -7,6 +7,6 @@ function getRepositories(el) {
   console.log(el);
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories)
-  req.open('GET', `https://api.github.com/users/${el}/repos`);
+  req.open('GET', `https://api.github.com/users/${el.input.value}/repos`);
   req.send();
 }
