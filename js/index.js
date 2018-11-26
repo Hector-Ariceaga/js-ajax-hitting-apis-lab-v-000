f06abd4aeeda7043c54be8c76a0cece680306791
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-26 22:39:59 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ function getCommits(el) {
 
 function displayBranches() {
   const branches = JSON.parse(this.responseText)
-  console.log(branches)
+  console.log(branches.message)
   if (branches.message === "Not Found") {
   const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>' ).join('')}</ul>`
   }
