0d9035b096c36b62471bdd76676b7a1af9c8971b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-26 21:20:23 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function displayRepositories() {
 }
 
 function getRepositories() {
-  var username = document.getElementById('username').value
+  var username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories)
   req.open('GET', `https://api.github.com/users/${username}/repos`);
@@ -17,6 +17,9 @@ function displayCommits() {
 }
 
 function getCommits(el) {
-  const name = el.dataset.name
+  const name = el.dataset.name;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/)
   
 }
