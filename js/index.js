35e1f021d4c58bfd549e2832397ca4c742230e1a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-26 21:03:52 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos)
-  const repoList = `<ul>${repos.map(repo => '<li>' + repo.name +)
+  const repoList = `<ul>${repos.map(repo => '<li>' + repo.name + '</li>').join('')}</ul>`
 }
 
 function getRepositories() {
