61cb867d6e36677b25719866bf4765013ed26b7d
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-26 21:29:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ function displayCommits() {
 }
 
 function getCommits(el) {
+  console.log(el)
   const name = el.dataset.repo;
   const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
